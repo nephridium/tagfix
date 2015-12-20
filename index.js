9a1c3eb21a060eec5cdea44bb098a1048bf167c3
nephridium <nephridium.labs@gmail.com>
2015-12-20 21:34:03 +0100

    Use stdin/stdout

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 var _ = require('lodash-node');
+var concat = require('concat-stream')
 
-var $in = cheerio.load(fs.readFileSync('2.html'));
+// var $input = cheerio.load(fs.readFileSync('2.html'));
 var tags = require('./tags');
 var validTagList = Object.keys(tags.valid);
 
@@ -10,29 +11,36 @@ var isValidTag = function(tag) {
   return validTagList.indexOf(tag.toLowerCase()) >= 0;
 }
 
-console.log($in.html());
+// read from stdin and write to stdout
+process.stdin.pipe(concat(function(data){
+  fix(cheerio.load(data.toString()));
+}))
 
-$in('*').map(function() {
-  if (this.type === 'tag') {
-    if (!isValidTag(this.name)) {
-      console.log('---', this.name);
-      var text = this.name;
-      if (this.attribs !== null) {
-        // append attributes
-        text = text + ' ' + Object.keys(this.attribs).join(' ');
-      }
-      if (this.children !== null) {
-        // append children text nodes
-        text = text + ' ' + _.map(this.children, function(el) {
-          return el.data;
-        }).join(' ');
-      }
-      console.log(text);
+var fix = function($in) {
+  // console.log($in.html());
 
-      this.data = text;
-      this.type = 'text';
+  $in('*').map(function() {
+    if (this.type === 'tag') {
+      if (!isValidTag(this.name)) {
+        console.error('---', this.name);
+        var text = this.name;
+        if (this.attribs !== null) {
+          // append attributes
+          text = text + ' ' + Object.keys(this.attribs).join(' ');
+        }
+        if (this.children !== null) {
+          // append children text nodes
+          text = text + ' ' + _.map(this.children, function(el) {
+            return el.data;
+          }).join(' ');
+        }
+        console.error(text);
+
+        this.data = text;
+        this.type = 'text';
+      }
     }
-  }
-  return this;
-})
-console.log($in.html());
+    return this;
+  })
+  console.log($in.html());
+}
