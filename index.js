287f6faaef4ebe2db527907e06d841e1c4456507
nephridium <nephridium.labs@gmail.com>
2015-12-20 20:45:28 +0100

    Add valid HTML tag list

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ var cheerio = require('cheerio');
 var _ = require('lodash-node');
 
 var $in = cheerio.load(fs.readFileSync('2.html'));
-var x = $in('*');
+var tags = require('./tags');
+var validTagList = Object.keys(tags.valid);
+
+var isValidTag = function(tag) {
+  return validTagList.indexOf(tag.toLowerCase()) >= 0;
+}
+
 console.log($in.html());
 
 $in('*').map(function() {
   if (this.type === 'tag') {
-    if (this.name === 'this') {
+    if (!isValidTag(this.name)) {
       console.log('---', this.name);
       var text = this.name;
       if (this.attribs !== null) {
+        // append attributes
         text = text + ' ' + Object.keys(this.attribs).join(' ');
       }
       if (this.children !== null) {
+        // append children text nodes
         text = text + ' ' + _.map(this.children, function(el) {
           return el.data;
         }).join(' ');
